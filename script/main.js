cd11232fcdc4d583147d3857c61bbd11fde88dc6
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2024-02-24 11:32:17 +0500

    Add shadow to header menu

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
 let openMenu = document.querySelector('.header__sidebar--btn');
 let headerShade = document.querySelector('.header__shade');
 let headerSidebar = document.querySelector('.header__sidebar');
+let headerMenu = document.querySelector('.header__menu');
 
 
 openMenu.addEventListener('click', () => {
@@ -9,5 +10,14 @@ openMenu.addEventListener('click', () => {
 })
 headerShade.addEventListener('click', () => {
     headerShade.classList.remove('show');
-    headerSidebar.classList.remove('show');
+    headerSidebar.classList.remove('sidebar');
 })
+
+window.onscroll = () => {
+    if (window.pageYOffset > 0) {
+        headerMenu.classList.add('box__shadow')
+    }
+    else {
+        headerMenu.classList.remove('box__shadow')
+    }
+}
